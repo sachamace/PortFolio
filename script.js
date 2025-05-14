e7a58bf9c20d68c39cc0791326caea21c6780ca8
Sacha <sacha.mace@gmail.com>
2025-05-14 15:30:25 +0200

    first commit portfolio

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+    let isDarkMode = false;
+
+    function enableDarkMode() {
+        body.classList.add('dark-mode');
+        body.classList.remove('light-mode');
+        themeToggle.textContent = 'Mode Clair';
+        isDarkMode = true;
+        localStorage.setItem('theme', 'dark');
+    }
+
+    function enableLightMode() {
+        body.classList.add('light-mode');
+        body.classList.remove('dark-mode');
+        themeToggle.textContent = 'Mode Sombre';
+        isDarkMode = false;
+        localStorage.setItem('theme', 'light');
+    }
+
+    themeToggle.addEventListener('click', () => {
+        if (isDarkMode) {
+            enableLightMode();
+        } else {
+            enableDarkMode();
+        }
+    });
+
+    // Vérification du thème sauvegardé au chargement de la page
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        enableDarkMode();
+    }
+});
